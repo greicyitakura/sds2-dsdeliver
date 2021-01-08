d2c71b1e0b98448f475dfa828b1e9bce3bb79816
greicyitakura <greicyitakura@gmail.com>
2021-01-08 18:36:37 -0300

    Search order location

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 function formatPrice(price: number) {
     const formatter = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
-        currency: 'BRL',
+        currency: 'BRL'
     });
 
     return formatter.format(price);
@@ -18,12 +18,15 @@ function ProductCard ({product}: Props) {
     return (
         <div className="order-card-container">
             <h3 className="order-card-title">{product.name}</h3>
-            <img src={product.imageUri}
+            <img 
+             src={product.imageUri}
              className="order-card-image"
              alt={product.name}/>
+
             <h3 className="order-card-price">{formatPrice(product.price)}</h3>
+            
             <div className="order-card-description">
-                <h3>Descrção</h3>
+                <h3>Descrição</h3>
                 <p>
                     {product.description}
                 </p>
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
 import ProductsList from './ProductsList';
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
 
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+
     
 
     useEffect(() => {
@@ -21,6 +24,7 @@ function Orders() {
        <div  className="orders-container">
            <StepsHeader/>
            <ProductsList products={products}/>
+           <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
        </div>
     )
 }
